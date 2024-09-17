956deb72f94eae227573991aa1774f52d80e210c
rifqirayita8 <rifqirrd8@gmail.com>
2024-09-17 11:05:44 +0700

    bug

---
+++ lib/features/auth/presentation/pages/bug.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Bug extends StatelessWidget {
+  const Bug({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return contol gedi;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:my_practice_bloc/example/bloc_builder/bloc_builder_consumer_example.dart';
 import 'package:my_practice_bloc/example/dependency_injection_provider/counter_cubit_three.dart';
-import 'package:my_practice_bloc/example/dependency_injection_provider/di_provider_example.dart';
-import 'package:my_practice_bloc/example/kue_cubit/cubit_example.dart';
-import 'package:my_practice_bloc/example/stream/stream_example.dart';
 import 'package:my_practice_bloc/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:my_practice_bloc/features/auth/presentation/pages/login_page.dart';
 import 'package:my_practice_bloc/injection.dart';
